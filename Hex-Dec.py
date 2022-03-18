d9af941820e3b96de8e0a913a41133ece544101e
_StupidYou <91799709+Stupid4You@users.noreply.github.com>
2022-03-18 18:47:20 +0100

    Add files via upload

---
+++ Hex-Dec.py
@@ -0,0 +1,18 @@
+from math import *
+HexaDecimal = input("Enter the hexadecimal: ")
+decimal = 0
+ListHexaDecimals = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+Filter = []
+i = len(HexaDecimal)
+item_change = 0
+print("Calculating...")
+for letter in HexaDecimal:
+    i -= 1
+  for item in ListHexaDecimals:
+    if item == letter:
+      item_change = int(pow(16, i)* ListHexaDecimals.index(item))
+      Filter.append(item_change)
+for itemcount in Filter:
+  decimal += itemcount
+print("Done")
+print("The decimal: " + str(decimal))
